e0d47816cd8c422ae0eb62fb882127af05403685
webchick <drupal@webchick.net>
2019-05-22 12:09:06 -0700

    Initial attempt at a script to auto-create Drupal 7 site with top 50 modules as dependencies.

---
+++ script.sh
@@ -0,0 +1,68 @@
+# Get a copy of Drupal 7 and the top 50 contributed modules.
+# We clone from Git in order to get the latest code, which may or may not
+# correspond to the latest release.
+git clone --branch 7.x https://git.drupalcode.org/project/drupal.git d7top50
+cd d7top50
+
+# Awww yeah, using MAMP like it's 1999... ;)
+drush8 si --db-url=mysql://root:root@127.0.0.1:8889/d7top50 -y
+
+MODULES="ctools		\
+views			\
+token			\
+libraries		\
+pathauto		\
+entity			\
+jquery_update		\
+admin_menu		\
+webform			\
+date			\
+metatag			\
+imce			\
+ckeditor		\
+module_filter		\
+link			\
+wysiwyg			\
+field_group		\
+rules			\
+transliteration		\
+entityreference		\
+captcha			\
+xmlsitemap		\
+colorbox		\
+media			\
+features		\
+backup_migrate		\
+views_bulk_operations	\
+variable		\
+file_entity		\
+views_slideshow		\
+panels			\
+l10n_update		\
+menu_block		\
+i18n			\
+devel			\
+globalredirect		\
+field_collection	\
+redirect		\
+context			\
+block_class		\
+imce_wysiwyg		\
+strongarm		\
+ds			\
+menu_attributes		\
+mailsystem		\
+email			\
+superfish		\
+addressfield		\
+admin_views"
+
+
+# Download and enable the top 50 contributed modules.
+drush8 dl -y $MODULES
+drush8 en -y $MODULES
+
+# Google Analtyics is a bit "special" since the project name and module name
+# mismatch.
+drush8 dl -y google_analytics 
+drush8 en -y googleanalytics
